ff7b8db6e2e4df49ac232ee82d2db2c84d53a534
Ben Desmarez <ben.desmarez@frizbiz.com>
2016-01-25 16:05:34 +0100

    Add family name to Companies

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125143744) do
+ActiveRecord::Schema.define(version: 20160125150457) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name",            limit: 255
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160125143744) do
     t.string   "mobile",          limit: 255
     t.string   "fax",             limit: 255
     t.string   "email",           limit: 255
+    t.string   "family_name",     limit: 255
   end
 
   create_table "contacts", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,7 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 100.times do
-  Family.create(name: Faker::Lorem.sentence(4),
-                description: Faker::Lorem.sentence(12, true, 10),
-                long_description: Faker::Lorem.paragraph
+  Company.create(name: Faker::Lorem.sentence(4),
+                description: Faker::Lorem.sentence(12, true, 10)
                )
 end
