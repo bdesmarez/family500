50f35837e9618e84624e03acc5461731b117651c
Ben Desmarez <ben.desmarez@frizbiz.com>
2016-01-25 18:22:28 +0100

    Add puma config

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 # Home controller
 class HomeController < ApplicationController
   def index
-    console
     @q = Company.ransack(params[:q])
     @q.sorts = 'rankings_turnover desc' if @q.sorts.empty?
     @companies = @q.result.includes(:rankings).paginate(page: params[:page])
